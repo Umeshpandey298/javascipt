5b7972ec731aa1080629df2f3e4f90ccdc8f720c
Umeshpandey298 <96909485+Umeshpandey298@users.noreply.github.com>
2024-09-02 06:05:59 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "umesh"
+
+// console.log(typeof(score))
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1; false => 0
+
+
+let isLoggedIn = 'umesh'
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 =>  true; 0 => false
+// "" => false
+// 'umesh' => true
+
+
+let someNumber = 333
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber));
+
+
