be1f75d6010a8dd2ed48277d460896fd2eb6893a
Umeshpandey298 <96909485+Umeshpandey298@users.noreply.github.com>
2024-09-02 05:33:49 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountId=144553
+let accountEmail="umeshpandey298@gmail.com"
+var accountPassword="12345"
+accountCity="Haldwani"
+
+let accountState;
+
+// accountId = 2  //not allowed
+
+
+accountEmail="umesh298@gmail.com"
+accountPassword="11111111111"
+accountCity="Halduchaur"
+
+console.log(accountId);
+
+/*
+prefer not  to use var
+because of issue in block scope and function scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,36 @@
+"use strict";// treat all JS code as vewer version
+
+//alert(3+3) //we are using nodejs not browser
+
+console.log(3
+    +
+
+    3 )  // code readability shoud be high
+
+console.log("umesh")
+
+let name = "umesh"
+let age = 18
+let isLoggedIn=false
+let state=null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefine => value is not assign
+// symbol => unique
+
+
+
+
+// object
+
+console.log(typeof null)  // object
+
+
+console.log(typeof string)  // undefine
+console.log(typeof boolean)  // undefine
+console.log(typeof undefine)  // undefine
+
