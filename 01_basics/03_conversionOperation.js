81fb98b0e8431c4a3a384178a1adf35e23f7f972
Umeshpandey298 <96909485+Umeshpandey298@users.noreply.github.com>
2024-09-02 06:40:57 +0000

    confusion part in conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,38 @@ let stringNumber = String(someNumber)
 console.log(typeof(stringNumber));
 
 
+//********************************operation******************************** */
+
+let value=3
+let navValue = -value
+console.log(navValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="hello"
+let str2=" umesh"
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1" +2)
+// console.log(1 +"2")
+// console.log("1" +2+2)
+// console.log(1 +2+'2')
+
+// console.log((3+4)*5%3) ;
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter=100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter)
+
+
