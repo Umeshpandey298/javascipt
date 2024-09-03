8eb3224b480578c19c90cb99cb963cb95842e0c7
Umeshpandey298 <96909485+Umeshpandey298@users.noreply.github.com>
2024-09-03 04:17:04 +0000

    Maths and Num is added

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,39 @@
+const score = 400
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString.length);
+console.log(balance.toFixed(2));
+
+
+const otherNumber=2311.2331
+console.log(otherNumber.toFixed(2));
+console.log(otherNumber.toPrecision(4))
+
+// ++++++++++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+console.log(Math.min(2,6,3,4,7,1,0,-1));
+console.log(Math.max(2,6,3,4,7,1,0,-1));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10))
+const min=10
+const max=20
+const x=Math.floor(Math.random()* (max-min+1)+min)
+console.log(x)
+
+
+
+
+
+
+
