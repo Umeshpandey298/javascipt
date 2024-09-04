815d2e1741afe592987a3a57ae541babed85fff8
Umeshpandey298 <96909485+Umeshpandey298@users.noreply.github.com>
2024-09-04 07:33:29 +0000

    flow control in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,24 @@
+// if
+
+
+// const isUserloggedin=true
+// const temp=41;
+
+
+// if(temp<50){
+//     console.log("less then 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+
+// <,> ,<=,>=,==,!=,===,!==
+
+
+// const score =200
+// if (score>100){
+//     var power="fly"
+//     console.log(`user power :${power}`);
+    
+// }
+// console.log(`user power :${power}`);
+
