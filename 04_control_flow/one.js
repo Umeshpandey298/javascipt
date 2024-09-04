074426b97989fc7c62efa9871754d308d53d57b6
Umeshpandey298 <96909485+Umeshpandey298@users.noreply.github.com>
2024-09-04 17:27:27 +0000

    .

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -22,3 +22,29 @@
 // }
 // console.log(`user power :${power}`);
 
+// const balance =1000
+// if(balance>500) console.log("test"),console.log("test2");
+
+// nesting in if else condition statement
+// if (balance<500){
+//     console.log("less then 500");
+// }else if(balance<750){
+//     console.log("less then 750");
+// }else if (balance<900){
+//     console.log("less then 900")
+// }
+
+// else {
+//     console.log("less then 1200");
+    
+// }
+
+const userloggedin=true
+const debitcard=true
+const loggedinfromgoogle=false
+const loggedinfromemail=true
+
+if(userloggedin&&debitcard&&2==3){
+    console.log("Allow to buy course");
+}
+
